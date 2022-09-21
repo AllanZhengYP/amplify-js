130919eff9d0dfbe5eb9ca75343879ff6fed8070
Dustin Noyes <dnnoyes@amazon.com>
2022-09-21 15:55:37 -0700

    cognito and core deps

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,19 +1,41 @@
-import { appendToCognitoUserAgent } from 'amazon-cognito-identity-js';
-
 import {
 	CookieStorage,
 	CognitoUserPool,
 	AuthenticationDetails,
 	ICognitoUserPoolData,
 	ICognitoUserData,
-	CognitoUser,
 	IAuthenticationCallback,
 	ICognitoUserAttributeData,
 	CognitoIdToken,
 	CognitoRefreshToken,
 	CognitoAccessToken,
+	ISignUpResult,
+	CognitoUser,
+	MFAOption,
+	CognitoUserSession,
+	CognitoUserAttribute,
+	NodeCallback,
+	ICookieStorageData,
+	ICognitoStorage,
+	appendToCognitoUserAgent,
 } from 'amazon-cognito-identity-js';
 
+import { Credentials, CredentialsClass } from '@aws-amplify/core';
+
 export function dontShakeOut() {
+	// amazon-cognito-identity-js
+	console.log(CookieStorage);
+	console.log(CognitoUserPool);
+	console.log(AuthenticationDetails);
+	console.log(CognitoIdToken);
+	console.log(CognitoRefreshToken);
+	console.log(CognitoAccessToken);
+	console.log(CognitoUser);
+	console.log(CognitoUserSession);
+	console.log(CognitoUserAttribute);
 	console.log(appendToCognitoUserAgent);
+
+	// @aws-amplify/core
+	console.log(Credentials);
+	console.log(CredentialsClass);
 }
