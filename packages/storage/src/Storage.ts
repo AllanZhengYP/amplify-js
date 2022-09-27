514013b79f5ec4a49d517ba0670f9ef6f1322041
Jim Blanchard <jablanch@amazon.com>
2022-09-27 08:07:32 -0500

    Fixed singleton registration.

--- packages/storage/src/Storage.ts
+++ packages/storage/src/Storage.ts
@@ -406,6 +406,41 @@ export class Storage {
 }
 
 /**
+ * Configure & register Storage singleton instance.
+ */
+let _instance: Storage = null;
+const getInstance = () => {
+	if (_instance) {
+		return _instance;
+	}
+	loggerStorageInstance.debug('Create Storage Instance, debug');
+	_instance = new Storage();
+	_instance.vault = new Storage();
+
+	const old_configure = _instance.configure;
+	_instance.configure = options => {
+		loggerStorageInstance.debug('storage configure called');
+		const vaultConfig = { ...old_configure.call(_instance, options) };
+
+		// set level private for each provider for the vault
+		Object.keys(vaultConfig).forEach(providerName => {
+			if (typeof vaultConfig[providerName] !== 'string') {
+				vaultConfig[providerName] = {
+					...vaultConfig[providerName],
+					level: 'private',
+				};
+			}
+		});
+		loggerStorageInstance.debug('storage vault configure called');
+		_instance.vault.configure(vaultConfig);
+	};
+	return _instance;
+};
+
+export const StorageInstance: Storage = getInstance();
+Amplify.register(StorageInstance);
+
+/**
  * @deprecated use named import
  */
 export default Storage;
