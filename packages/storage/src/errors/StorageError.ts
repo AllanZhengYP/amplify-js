0bd42779787798ec8021fe9f49e8796b1712561d
Allan Zheng <zheallan@amazon.com>
2023-07-29 16:10:51 -0700

    feat(storage): add StorageError and credentials validation

---
+++ packages/storage/src/errors/StorageError.ts
@@ -0,0 +1,27 @@
+import {
+	AmplifyError,
+	ConsoleLogger as Logger,
+	ErrorParams,
+} from '@aws-amplify/core';
+
+export class StorageError extends AmplifyError {
+	constructor(params: ErrorParams) {
+		super(params);
+
+		// TODO: Delete the following 2 lines after we change the build target to >= es2015
+		this.constructor = StorageError;
+		Object.setPrototypeOf(this, StorageError.prototype);
+	}
+}
+
+export enum StorageErrorTypes {
+	NO_CREDENTIALS = 'NO_CREDENTIALS',
+}
+
+const StorageErrorStrings: { [key in StorageErrorTypes]: ErrorParams } = {
+	[StorageErrorTypes.NO_CREDENTIALS]: {
+		name: 'NoCredentials',
+		message: 'No credentials',
+		recoverySuggestion: ``,
+	},
+};
